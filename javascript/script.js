3f6acfa33865ce7dddbe1fae7a5be11981678762
Steven Elliott <steven.elliott@live.com>
2021-02-03 16:18:08 -0500

    added a screenshot for the readme page

--- javascript/script.js
+++ javascript/script.js
@@ -36,6 +36,7 @@ $(document).ready(function() {
 
                     $(".uvI").text("UV Index: " + uvData.value);
                     $(".uvI").css("background-color", "yellow");
+                        
                 })
             
         })
